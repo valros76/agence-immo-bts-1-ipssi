5883403d8aaa1d3676a0500d8f9718ee2d0110e7
valros76 <valerian.dufrene@gmail.com>
2025-04-01 10:39:12 +0200

    Refonte autoloader

--- utils/Autoloader.php
+++ utils/Autoloader.php
@@ -14,6 +14,16 @@ class Autoloader{
         "Utils"
       ];
 
+      foreach($dirs as $dir){
+        if(str_contains($class, $dir)){
+          $type = lcfirst($dir);
+          $class = str_replace("{$dir}/", "", $class);
+        }
+        if($type && file_exists("{$type}/{$class}.php")){
+          require_once "{$type}/{$class}.php";
+        }
+      }
+
       if(file_exists("controllers/{$class}.php")){
         require_once "controllers/{$class}.php";
       }
